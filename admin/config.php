7f0febaa08cb7545be3e5a2270d1a0c2279505ce
shmakovdima <contact@shmakovdima.ru>
2016-02-17 16:41:19 +0300

    true

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://weblama.me/admin/');
+define('HTTP_CATALOG', 'http://weblama.me/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://weblama.me/admin/');
+define('HTTPS_CATALOG', 'http://weblama.me/');
+
+// DIR
+define('DIR_APPLICATION', '/home/f/fb7903js/weblame.me/public_html/admin/');
+define('DIR_SYSTEM', '/home/f/fb7903js/weblame.me/public_html/system/');
+define('DIR_LANGUAGE', '/home/f/fb7903js/weblame.me/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/f/fb7903js/weblame.me/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/f/fb7903js/weblame.me/public_html/system/config/');
+define('DIR_IMAGE', '/home/f/fb7903js/weblame.me/public_html/image/');
+define('DIR_CACHE', '/home/f/fb7903js/weblame.me/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/f/fb7903js/weblame.me/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/f/fb7903js/weblame.me/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/f/fb7903js/weblame.me/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/f/fb7903js/weblame.me/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/f/fb7903js/weblame.me/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'fb7903js_moda');
+define('DB_PASSWORD', 'as210100');
+define('DB_DATABASE', 'fb7903js_moda');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ma_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://weblama.me/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://weblama.me/');
+
+// DIR
+define('DIR_APPLICATION', '/home/f/fb7903js/weblame.me/public_html/catalog/');
+define('DIR_SYSTEM', '/home/f/fb7903js/weblame.me/public_html/system/');
+define('DIR_LANGUAGE', '/home/f/fb7903js/weblame.me/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/f/fb7903js/weblame.me/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/f/fb7903js/weblame.me/public_html/system/config/');
+define('DIR_IMAGE', '/home/f/fb7903js/weblame.me/public_html/image/');
+define('DIR_CACHE', '/home/f/fb7903js/weblame.me/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/f/fb7903js/weblame.me/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/f/fb7903js/weblame.me/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/f/fb7903js/weblame.me/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/f/fb7903js/weblame.me/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'fb7903js_moda');
+define('DB_PASSWORD', 'as210100');
+define('DB_DATABASE', 'fb7903js_moda');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ma_');
